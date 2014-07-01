df99c5bbfc5d5622650c530b96df6c14f4a80400
setetres <setetres@gmail.com>
2014-07-01 20:20:38 -0300

    Fix

--- js/console.js
+++ js/console.js
@@ -76,7 +76,6 @@ function (cmdLineContainer, outputContainer) {
                 var args = this.value.split(' ').filter(function (val, i) {
                     window.top.document.getElementById('soundtrack-type').play();
                     return val;
-                    $('a[rel=external]').attr('target', '_blank');
                 });
                 var cmd = args[0].toLowerCase();
                 args = args.splice(1);
@@ -86,13 +85,16 @@ function (cmdLineContainer, outputContainer) {
             // about
 
             case 'about':
-                output("<pre class='hello'>██╗  ██╗███████╗██╗     ██╗      ██████╗ <br/>██║  ██║██╔════╝██║     ██║     ██╔═══██╗<br/>███████║█████╗  ██║     ██║     ██║   ██║<br/>██╔══██║██╔══╝  ██║     ██║     ██║   ██║<br/>██║  ██║███████╗███████╗███████╗╚██████╔╝<br/>╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝ </pre> <p>My name is Guilherme Pangnotta.<br/>I'm a web developer and web designer from Belo Horizonte, Brazil.</p>");
+                output("<pre class='hello'>██╗  ██╗███████╗██╗     ██╗      ██████╗ <br/>██║  ██║██╔════╝██║     ██║     ██╔═══██╗<br/>███████║█████╗  ██║     ██║     ██║   ██║<br/>██╔══██║██╔══╝  ██║     ██║     ██║   ██║<br/>██║  ██║███████╗███████╗███████╗╚██████╔╝<br/>╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝ ╚═════╝ </pre><p>My name is Guilherme Pangnotta.<br/>I'm a web developer and web designer from Belo Horizonte, Brazil.</p><p>View <a href='http://go.setetres.st/cv' rel='external'>curriculum vitae</a>.</p>");
+                $('a[rel=external]').attr('target', '_blank');
                 break;
 
             // awards
 
             case 'awards':
-                output("<pre>┌─┐┬ ┬┌─┐┬─┐┌┬┐┌─┐<br/>├─┤│││├─┤├┬┘ ││└─┐<br/>┴ ┴└┴┘┴ ┴┴└──┴┘└─┘</pre> <ul> <li><strong>Awwwards</strong> _ Sete Três v1 / <i>Site of the Day</i></li> <li><strong>CSS Awards</strong> _ Sete Três v1 / <i>Site of the Day</i></li> <li><strong>Design Taxi</strong> _ Sete Três v1 / <i>Site of the Day</i></li> <li><strong>The FWA</strong> _ Sete Três v1 / <i>Public Shortlist</i></li> <li><strong>One Page Love</strong> _ InkStarter.cc / <i>Site of the Day</i></li> <li><strong>Popai Brasil 2013</strong> _ Citibank Totem / <i>Gold on Digital Signage</i> via <a href='http://jchebly.com.br' rel='external'>JCHEBLY</a></li> </ul>"); break;
+                output("<pre>┌─┐┬ ┬┌─┐┬─┐┌┬┐┌─┐<br/>├─┤│││├─┤├┬┘ ││└─┐<br/>┴ ┴└┴┘┴ ┴┴└──┴┘└─┘</pre><ul><li>Site of the Day @ Awwwards / <i>Sete Três v1</i></li><li>Site of the Day @ CSS Awards <i>Sete Três v1</i></li><li>Site of the Day @ Design Taxi / <i>Sete Três v1</i></li><li>Public Shortlist @ The FWA / <i>Sete Três v1</i></li><li>Site of the Day @ One Page Love / <i>InkStarter.cc</i></li><li>Gold - Digital Signage @ Popai Brasil 2013 / <i>Citibank</i></li></ul>");
+                $('a[rel=external]').attr('target', '_blank');
+                break;
 
             // clear
 
@@ -111,17 +113,21 @@ function (cmdLineContainer, outputContainer) {
 
             case 'help':
                 output("<div class='ls-files'><p>" + CMDS_.join('<br/>') + "</p></div>");
-                break;
+            break;
 
             // projects
 
             case 'projects':
-                output("<pre>┌─┐┌─┐┬─┐┌─┐┌─┐┌┐┌┌─┐┬    ┌─┐┬─┐┌─┐ ┬┌─┐┌─┐┌┬┐┌─┐<br/>├─┘├┤ ├┬┘└─┐│ ││││├─┤│    ├─┘├┬┘│ │ │├┤ │   │ └─┐<br/>┴  └─┘┴└─└─┘└─┘┘└┘┴ ┴┴─┘  ┴  ┴└─└─┘└┘└─┘└─┘ ┴ └─┘</pre> <ul> <li><a href='http://placevom.it' rel='external'>Placevom.it</a> <i>Image placeholder for disgusting layouts. <sup>NEW</sup></i></li> <li><a href='http://inkstarter.cc' rel='external'>InkStarter.cc</a> <i>You draw. You vote. I tattoo myself!</i></li> <li><a href='http://1997.setetres.st' rel='external'>1997</a> <i>Feeling responsively nostalgic?</i></li> <li><a href='http://v1.setetres.st' rel='external'>v1</a> <i>Award winning v1.0 of my website.</i></li> <li><a href='http://v2.setetres.st' rel='external'>v2</a> <i>v2.0 of my website.</i></li> <li><a href='http://go.setetres.st/starter' rel='external'>Starter</a> <i>A quick-start responsive framework for frontend development.</i></li> <li><a href='http://go.setetres.st/sasstarter' rel='external'>Sasstarter</a> <i>A quick-start responsive framework for frontend development with Sass.</i></li> <li><a href='http://puta.nu' rel='external'>PUTA.nu</a> <i>Porn Under Traditional Arts <sup>SOON</sup></i></li> </ul> <pre>┌─┐┬─┐┌─┐┌─┐┌─┐┌─┐┌─┐┬┌─┐┌┐┌┌─┐┬    ┌─┐┬─┐┌─┐ ┬┌─┐┌─┐┌┬┐┌─┐<br/>├─┘├┬┘│ │├┤ ├┤ └─┐└─┐││ ││││├─┤│    ├─┘├┬┘│ │ │├┤ │   │ └─┐<br/>┴  ┴└─└─┘└  └─┘└─┘└─┘┴└─┘┘└┘┴ ┴┴─┘  ┴  ┴└─└─┘└┘└─┘└─┘ ┴ └─┘</pre> <ul> <li><a href='http://cemporcentomp.com.br' rel='external'>100% Marca Própria</a> <i>HTML + CSS + JS</i> via <a href='http://jchebly.com.br' rel='external'>JCHEBLY</a></li> <li><a href='http://newtonpaiva.br' rel='external'>Newton</a> <i>HTML + CSS + JS</i> via <a href='http://jchebly.com.br' rel='external'>JCHEBLY</a></li> <li><a href='http://belo2014.com.br' rel='external'>Belo 2014</a> <i>HTML + CSS + JS</i> via <a href='http://jchebly.com.br' rel='external'>JCHEBLY</a></li> <li><a href='http://jchebly.com.br' rel='external'>JCHEBLY</a> <i>HTML + CSS + JS</i> via <a href='http://jchebly.com.br' rel='external'>JCHEBLY</a></li> <li><a href='http://dmsports.co' rel='external'>DM Sports</a> <i>HTML + CSS + JS + Wordpress</i></li> <li><a href='http://dsmais.com.br' rel='external'>DS+</a> <i>HTML + CSS + JS + Wordpress</i></li> </ul>"); break;
+                output("<pre>┌─┐┌─┐┬─┐┌─┐┌─┐┌┐┌┌─┐┬    ┌─┐┬─┐┌─┐ ┬┌─┐┌─┐┌┬┐┌─┐<br/>├─┘├┤ ├┬┘└─┐│ ││││├─┤│    ├─┘├┬┘│ │ │├┤ │   │ └─┐<br/>┴  └─┘┴└─└─┘└─┘┘└┘┴ ┴┴─┘  ┴  ┴└─└─┘└┘└─┘└─┘ ┴ └─┘</pre><ul><li><a href='http://placevom.it' rel='external'>Placevom.it</a> / <i>Image placeholder for disgusting layouts.</i></li><li><a href='http://inkstarter.cc' rel='external'>InkStarter.cc</a> / <i>You draw. You vote. I tattoo myself!</i></li><li><a href='http://1997.setetres.st' rel='external'>1997</a> / <i>Feeling responsively nostalgic?</i></li><li><a href='http://v1.setetres.st' rel='external'>v1</a> / <i>Award winning v1.0 of my website.</i></li><li><a href='http://v2.setetres.st' rel='external'>v2</a> / <i>v2.0 of my website.</i></li><li><a href='http://go.setetres.st/starter' rel='external'>Starter</a> / <i>A quick-start responsive framework for frontend development.</i></li><li><a href='http://go.setetres.st/sasstarter' rel='external'>Sasstarter</a> / <i>A quick-start responsive framework for frontend development with Sass.</i></li><li><a href='http://puta.nu' rel='external'>PUTA.nu</a> / <i>Porn Under Traditional Arts™</i></li></ul><pre>┌─┐┬─┐┌─┐┌─┐┌─┐┌─┐┌─┐┬┌─┐┌┐┌┌─┐┬    ┌─┐┬─┐┌─┐ ┬┌─┐┌─┐┌┬┐┌─┐<br/>├─┘├┬┘│ │├┤ ├┤ └─┐└─┐││ ││││├─┤│    ├─┘├┬┘│ │ │├┤ │   │ └─┐<br/>┴  ┴└─└─┘└  └─┘└─┘└─┘┴└─┘┘└┘┴ ┴┴─┘  ┴  ┴└─└─┘└┘└─┘└─┘ ┴ └─┘</pre><ul><li><a href='http://cemporcentomp.com.br' rel='external'>100% Marca Própria</a> / HTML + CSS + JS @ <i>JCHEBLY</i></li><li><a href='http://newtonpaiva.br' rel='external'>Newton</a> / HTML + CSS + JS @ <i>JCHEBLY</i></li><li><a href='http://belo2014.com.br' rel='external'>Belo 2014</a> / HTML + CSS + JS @ <i>JCHEBLY</li><li><a href='http://jchebly.com.br' rel='external'>JCHEBLY</a> / HTML + CSS + JS @ <i>JCHEBLY</li><li><a href='http://dmsports.co' rel='external'>DM Sports</a> / HTML + CSS + JS + Wordpress @ <i>Sete Três</i></li> <li><a href='http://dsmais.com.br' rel='external'>DS+</a> / HTML + CSS + JS + Wordpress @ <i>Sete Três</i></li></ul>");
+                $('a[rel=external]').attr('target', '_blank');
+            break;
 
             // resume
 
             case 'resume':
-                output("<pre>┌─┐┬ ┬┬─┐┬─┐┌─┐┌┐┌┌┬┐<br/>│  │ │├┬┘├┬┘├┤ │││ │ <br/>└─┘└─┘┴└─┴└─└─┘┘└┘ ┴ </pre> <p><strong>Frontend Developer</strong> @ <a href='http://jchebly.com.br' rel='external'>JCHEBLY</a> <i>Since September 2010</i></p> <pre>┌─┐┌─┐┌─┐┌┬┐<br/>├─┘├─┤└─┐ │ <br/>┴  ┴ ┴└─┘ ┴ </pre> <ul> <li><strong>AgênciaClick Isobar</strong> <i>June 2009 – August 2010</i></li> <li><strong>Sense8</strong> <i>September 2008 – May 2009</i></li> <li><strong>HouseID</strong> <i>September 2007 – May 2008</i></li> </ul> <pre>┌─┐┌┬┐┬ ┬┌─┐┌─┐┌┬┐┬┌─┐┌┐┌<br/>├┤  │││ ││  ├─┤ │ ││ ││││<br/>└─┘─┴┘└─┘└─┘┴ ┴ ┴ ┴└─┘┘└┘</pre> <p><strong>Advertising</strong> @ <a href='http://newtonpaiva.br' rel='external'>Newton</a></p>"); break;
+                output("<pre>┌─┐┬ ┬┬─┐┬─┐┌─┐┌┐┌┌┬┐<br/>│  │ │├┬┘├┬┘├┤ │││ │ <br/>└─┘└─┘┴└─┴└─└─┘┘└┘ ┴ </pre><ul><li>Tattoo Artist @ <a href='http://inkstarter.cc' rel='external'>InkStarter.cc</a> / <i>Since March 2014</i></li><li>Frontend Developer @ <a href='http://setetres.st' rel='external'>Sete Três</a> / <i>Since January 2008</i></li><li>Frontend Developer @ <a href='http://jchebly.com.br' rel='external'>JCHEBLY</a> / <i>Since September 2010</i></li></ul><pre>┌─┐┌─┐┌─┐┌┬┐<br/>├─┘├─┤└─┐ │ <br/>┴  ┴ ┴└─┘ ┴ </pre><ul><li>Frontend Developer @ AgênciaClick Isobar / <i>June 2009 – August 2010</i></li><li>Frontend Developer @ Sense8 / <i>September 2008 – May 2009</i></li> <li>Frontend Developer @ HouseID / <i>September 2007 – May 2008</i></li></ul><pre>┌─┐┌┬┐┬ ┬┌─┐┌─┐┌┬┐┬┌─┐┌┐┌<br/>├┤  │││ ││  ├─┤ │ ││ ││││<br/>└─┘─┴┘└─┘└─┘┴ ┴ ┴ ┴└─┘┘└┘</pre><p>Advertising @ <a href='http://newtonpaiva.br' rel='external'>Newton</a> / <i>January 2006 – December 2010</i></p>");
+                $('a[rel=external]').attr('target', '_blank');
+            break;
 
             // theme
 
@@ -137,14 +143,14 @@ function (cmdLineContainer, outputContainer) {
                         output("<p>" + "Error - Unrecognized theme used" + "</p>");
                     }
                 }
-                break;
+            break;
 
             // version
 
             case 'version':
             case 'ver':
                 output("<p>" + VERSION_ + "</p>");
-                break;
+            break;
 
             // command not found
 
